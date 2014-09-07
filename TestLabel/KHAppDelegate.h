4b37c282c353c40e8d16a13e8f0df0c5ba5863ef
milichenko <milichenko@maximagroup.com>
2014-09-07 19:41:23 +0300

    Initial Commit

---
+++ TestLabel/KHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KHAppDelegate.h
+//  TestLabel
+//
+//  Created by Vladimir Milichenko on 9/7/14.
+//  Copyright (c) 2014 Vladimir Milichenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestLabel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestLabel
+//
+//  Created by Vladimir Milichenko on 9/7/14.
+//  Copyright (c) 2014 Vladimir Milichenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KHAppDelegate class]));
+    }
+}
